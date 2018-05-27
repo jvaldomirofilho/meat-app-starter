40af9a1034a7b6b8e5acafee7f9a611211b2e103
Valdomiro Filho <valdomirofilho@unicatolicaquixada.edu.br>
2018-05-27 00:40:36 -0300

    aula 51 - add menu item

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 import { ErrorHandler } from '../app.error-handler';
+import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model';
 
 
 @Injectable()
@@ -30,4 +31,20 @@ export class RestaurantsService {
                         .map(response => response.json())
                         .catch(ErrorHandler.handleError)
     }
+
+    reviewsOfRestaurant(id: string): Observable<any>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+                        .map(response => response.json())
+                        .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+                        .map(response => response.json())
+                        .catch(ErrorHandler.handleError)
+    }
+
+
+
+
 }
